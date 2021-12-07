818b9a4e2d04cc8ea946a8c3e08d90b998cc9c78
Error1368 <www.exuisitor@gmail.com>
2021-12-07 13:27:03 +0300

    init commit and add classes

---
+++ Classes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classess
+{
+    public interface IBuilding
+    {
+
+    }
+
+    public abstract class PublicBuilding : IBuilding
+    {
+    
+    }
+
+    public class Theatre : PublicBuilding
+    { 
+    
+    }
+}
